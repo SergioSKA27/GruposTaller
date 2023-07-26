87041340bdc963edd3d897d3dc7ec1b013a45a16
SergioSKA27 <sergioska81@hotmail.com>
2023-07-26 03:46:31 -0600

    More stuff :)

---
+++ pages/Herstein.py
@@ -0,0 +1,26 @@
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
+import os
+import base64
+@st.cache
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+
+    return pdf_display
+
+
+
+st.title('Algebra Moderna Herstein')
+
+p = displayPDF("pages/Algebra_Moderna_Herstein.pdf")
+
+st.markdown(p, unsafe_allow_html=True)
